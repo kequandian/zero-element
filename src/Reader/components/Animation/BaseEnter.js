37cb6aeec6b1f658e42abc28c9f6a51bc8093483
LLH <youyeke@163.com>
2019-05-17 17:15:54 +0800

    淡入效果

--- src/Reader/components/Animation/BaseEnter.js
+++ src/Reader/components/Animation/BaseEnter.js
@@ -4,15 +4,21 @@ import { useDidMount } from '@/utils/hooks/lifeCycle';
 
 export default function BaseEnter({ children }) {
   const [offsetY, setOffsetY] = useState(20);
+  const [opacity, setOpacity] = useState(0);
   useDidMount(_ => {
     setOffsetY(0);
+    setOpacity(1);
   });
 
-  return <Motion style={{ x: spring(offsetY, presets.noWobble) }}>
+  return <Motion style={{
+    x: spring(offsetY, presets.noWobble),
+    opacity: spring(opacity, presets.noWobble),
+  }}>
     {interpolatingStyle => {
       const style = {};
       if (interpolatingStyle.x !== 0) {
-        style.transform = `translateY(${interpolatingStyle.x}px)`
+        style.transform = `translateY(${interpolatingStyle.x}px)`;
+        style.opacity = interpolatingStyle.opacity;
       }
       return (
         <div style={style}>
