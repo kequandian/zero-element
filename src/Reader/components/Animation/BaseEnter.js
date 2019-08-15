9456bb58f2e6003e9b11e25025016e362bafb238
LLH <youyeke@163.com>
2019-08-15 15:01:44 +0800

    BaseEnter 传递 style

--- src/Reader/components/Animation/BaseEnter.js
+++ src/Reader/components/Animation/BaseEnter.js
@@ -5,6 +5,7 @@ import { useDidMount } from '@/utils/hooks/lifeCycle';
 export default function BaseEnter({ children }) {
   const [offsetY, setOffsetY] = useState(20);
   const [opacity, setOpacity] = useState(0);
+
   useDidMount(_ => {
     setOffsetY(0);
     setOpacity(1);
@@ -20,11 +21,9 @@ export default function BaseEnter({ children }) {
         style.transform = `translateY(${interpolatingStyle.x}px)`;
         style.opacity = interpolatingStyle.opacity;
       }
-      return (
-        <div style={style}>
-          {children}
-        </div>
-      )
+      return React.cloneElement(children, {
+        style,
+      });
     }}
   </Motion>
 }
