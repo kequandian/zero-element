6f7c3bae9275926eb28b31ccd7dd4ccb9b47f69d
LLH <youyeke@163.com>
2019-08-23 11:10:23 +0800

    封装 method download

--- src/utils/request/index.js
+++ src/utils/request/index.js
@@ -38,3 +38,42 @@ export async function remove(api) {
     },
   }).catch(error);
 }
+export async function download(api, { method = 'get', fileName }) {
+  return request({
+    url: api,
+    method,
+    baseURL: getEndpoint(),
+    responseType: 'blob',
+    headers: {
+      'Authorization': "Bearer " + getToken(),
+    },
+  })
+    .then(res => downloadFile(res, fileName))
+    .catch(error);
+}
+
+function downloadFile(res, defaultName = 'file') {
+  if (res.data.type === "application/json") {
+    return Promise.reject('api 未返回文件数据流');
+  } else {
+    const disposition = res.headers['content-disposition'] || '';
+    const matchRst = disposition.match(/filename="(\S+)"/i);
+    const fileName = matchRst && matchRst[1] || defaultName;
+
+    const blob = new Blob([res.data]);
+    if (window.navigator.msSaveOrOpenBlob) {
+      navigator.msSaveBlob(blob, fileName); //兼容ie10
+    } else {
+      const link = document.createElement("a");
+      const evt = document.createEvent("HTMLEvents");
+      evt.initEvent("click", false, false);
+      link.href = URL.createObjectURL(blob);
+      link.download = fileName;
+      link.style.display = "none";
+      document.body.appendChild(link);
+      link.click();
+      link.remove();
+      window.URL.revokeObjectURL(link.href);
+    }
+  }
+}
